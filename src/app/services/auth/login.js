ee74c8b10c5ffcac27823a366c181d24ffafd83d
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-10 03:38:15 -0500

    Add: Interaction with firebase working

--- src/app/services/auth/login.js
+++ src/app/services/auth/login.js
@@ -1,6 +1,13 @@
+/* eslint-disable */
 import Vue from 'vue';
 import accountService from './../account';
 import store from './../../store';
+let firebase = require('firebase');
+let app = firebase.initializeApp({apiKey: "AIzaSyD2-KL9ohgtMUZQi27QyoswmuWNsEA-t9Q",
+    authDomain: "busy-d727c.firebaseapp.com",
+    databaseURL: "https://busy-d727c.firebaseio.com",
+    storageBucket: "busy-d727c.appspot.com",
+    messagingSenderId: "555996787874"});
 
 // When the request succeeds
 const success = (token) => {
@@ -29,9 +36,33 @@ export default (user) => {
    *     failed(error);
    *   });
    */
-  if (!user.email || !user.password) {
-    failed();
-  } else {
-    success('RandomGeneratedToken');
-  }
+  // if (!user.email || !user.password) {
+  //   failed();
+  // } else {
+  //   success('RandomGeneratedToken');
+  // }
+  // var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.database().ref('users/').set({
+    username: "Iggy",
+    email: "Ig"
+  });
+
+  // firebase.auth().signInWithPopup(provider).then(function(result) {
+  //   // This gives you a Google Access Token. You can use it to access the Google API.
+  //   var token = result.credential.accessToken;
+  //   // The signed-in user info.
+  //   var user = result.user;
+  //   console.log(user);
+  //   // ...
+  //   }).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   // The email of the user's account used.
+  //   var email = error.email;
+  //   // The firebase.auth.AuthCredential type that was used.
+  //   var credential = error.credential;
+  //   // ...
+  // });
 };
