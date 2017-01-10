9292216087e5990d074b44817e70d145afd2b61f
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-09 23:54:14 -0500

    Add: Vue scaffold

---
+++ src/app/pages/home/index/index.js
@@ -0,0 +1,13 @@
+/* ============
+ * Home Index Page
+ * ============
+ *
+ * The home index page
+ */
+
+export default {
+  components: {
+    VLayout: require('layouts/default/default.vue'),
+    VPanel: require('components/panel/panel.vue'),
+  },
+};
