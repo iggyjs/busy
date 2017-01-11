ed9ac79a49d38d64a4784fc19f855f84977c15ac
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-11 11:03:20 -0500

    Add: Lists and add methods

--- src/app/pages/home/index/index.js
+++ src/app/pages/home/index/index.js
@@ -14,11 +14,30 @@ let app = firebase.initializeApp({apiKey: "AIzaSyD2-KL9ohgtMUZQi27QyoswmuWNsEA-t
 });
 
 export default {
+  data() {
+      return {
+        newDailyTodo: "",
+        newWeeklyTodo: "",
+        dailyId: 0,
+        weeklyId: 0,
+        dailyList: [],
+        weeklyList: []
+      }
+  },
   components: {
     VLayout: require('layouts/default/default.vue'),
     VPanel: require('components/panel/panel.vue'),
   },
   methods:{
-      
+      addDailyTodo(){
+          let id = this.dailyId;
+          this.dailyList.push({ uid : id, item: this.newDailyTodo});
+          this.dailyId++;
+      },
+      addWeeklyTodo(){
+          let id = this.weeklyId;
+          this.weeklyList.push({uid : id, item: this.newWeeklyTodo});
+          this.weeklyId++;
+      }
   }
 };
