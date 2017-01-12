d3037f112115c63c399707fc721c84213f608665
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-11 22:08:57 -0500

    Add: draggable

--- src/app/pages/home/index/index.js
+++ src/app/pages/home/index/index.js
@@ -4,6 +4,7 @@
  *
  * The home index page
  */
+import draggable from 'vuedraggable';
 import authService from 'src/app/services/auth';
 let firebase = require('firebase');
 let app = firebase.initializeApp({apiKey: "AIzaSyD2-KL9ohgtMUZQi27QyoswmuWNsEA-t9Q",
@@ -24,6 +25,7 @@ export default {
       }
   },
   components: {
+    draggable,
     VLayout: require('layouts/default/default.vue'),
     VPanel: require('components/panel/panel.vue'),
   },
