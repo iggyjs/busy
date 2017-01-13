4839fa95f8e64edfc18d1234e0b59894251b9b89
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-13 16:08:28 -0500

    Add: styling for login page

--- src/app/pages/login/index/index.js
+++ src/app/pages/login/index/index.js
@@ -1,33 +1,38 @@
-/* ============
- * Login Index Page
- * ============
- *
- * Page where the user can login
- */
-
+import lodash from 'lodash';
 import authService from './../../../services/auth';
 
 export default {
-
-  data() {
-    return {
-      user: {
-        email: null,
-        password: null,
-      },
-    };
-  },
-  created: function(){
-
-  },
-  methods: {
-    login(user) {
-      authService.login(user);
+    data() {
+        return {
+            window: window,
+            user: {
+                email: null,
+                password: null,
+            },
+            buttonStyles: [
+                {backgroundColor: "#6F83BA", color: "#FFFFFF", textShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)"}, //indigo
+                {backgroundColor: "#BBDEFB", color: "#FFFFFF", textShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)"}, //baby blue
+                {backgroundColor: "#FFD54F", color: "#FFFFFF", textShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)"}, //amber
+            ]
+        };
     },
-  },
 
-  components: {
-    VLayout: require('layouts/default/default.vue'),
-    VPanel: require('components/panel/panel.vue'),
-  },
+    created() {
+        
+    },
+
+    methods: {
+        login(user) {
+            authService.login(user);
+        },
+
+        random(min, max){
+            return _.random(min, max);
+        }
+    },
+
+    components: {
+        VLayout: require('layouts/default/default.vue'),
+        VPanel: require('components/panel/panel.vue'),
+    },
 };
