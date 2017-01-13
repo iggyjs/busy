eaaf37890c5e6bafd97832ec88faa601993fc56b
Ignacio Streuly <ijstreuly@gmail.com>
2017-01-13 13:31:35 -0500

    Fix: Undefined business

--- src/app/pages/home/index/index.js
+++ src/app/pages/home/index/index.js
@@ -1,4 +1,5 @@
 // TODO: Error handle empty submissions, quickly implement max items for list
+// FIXME: On user create handle creation of completed lists to avoid undefined errors
 
 import draggable from 'vuedraggable';
 import authService from 'src/app/services/auth';
@@ -19,7 +20,9 @@ export default {
         newDailyTodo: "",
         newWeeklyTodo: "",
         dailyList: [],
-        weeklyList: []
+        weeklyList: [],
+        dailyListCompleted : [],
+        weeklyListCompleted : []
       }
   },
 
@@ -31,8 +34,14 @@ export default {
            let id = u.uid;
            firebase.database().ref('users/' + id).once('value').then((snapshot) => {
                vueInstance.user = snapshot.val();
+               // set daily lists
                vueInstance.dailyList = snapshot.val().dailyList.list;
+               vueInstance.dailyListCompleted = snapshot.val().dailyList.completed.completedList;
+
+               // set weekly lists
                vueInstance.weeklyList = snapshot.val().weeklyList.list;
+               vueInstance.weeklyListCompleted = snapshot.val().weeklyList.completed.completedList;
+
            });
            return;
         }
@@ -49,10 +58,10 @@ export default {
   methods:{
       addDailyTodo(){
           let id = this.dailyId;
-          this.dailyList.push({ item: this.newDailyTodo});
+          this.dailyList.push({ item: this.newDailyTodo });
           this.newDailyTodo = "";
           this.dailyId++;
-          this.saveDailyItems()
+          this.saveDailyItems();
       },
 
       addWeeklyTodo(){
@@ -63,25 +72,29 @@ export default {
           this.saveWeeklyItems()
       },
 
-      deleteDailyItem(item){
+      markDailyItemCompleted(item){
           let index = this.dailyList.indexOf(item);
           if (index > -1) {
             this.dailyList.splice(index,1);
-            this.saveDailyItems()
           }
+          this.dailyListCompleted.push(item)
+          this.saveDailyItems();
       },
 
-      deleteWeeklyItem(item){
+      markWeeklyItemCompleted(item){
+
           let index = this.weeklyList.indexOf(item);
           if (index > -1){
             this.weeklyList.splice(index,1);
-            this.saveWeeklyItems();
           }
+          this.weeklyListCompleted.push(item)
+          this.saveWeeklyItems();
       },
 
       saveDailyItems(){
           let id = firebase.auth().currentUser.uid;
           let list = this.dailyList;
+          let completedList = this.dailyListCompleted;
 
           if (this.currentUserId == null){
               this.currentUserId = id;
@@ -89,13 +102,18 @@ export default {
 
           //save current daily list
           firebase.database().ref('users/' + id + '/dailyList/').set({list}).then(() => {
-              console.log("daily list saved");
+              console.log("Daily list saved");
+              // save currently daily completed list
+              firebase.database().ref('users/' + id + '/dailyList/completed/').set({completedList}).then(() =>{
+                  console.log("Daily completed list saved");
+              });
           });
       },
 
       saveWeeklyItems(){
           let id = firebase.auth().currentUser.uid;
           let list = this.weeklyList;
+          let completedList = this.weeklyListCompleted;
 
           if (this.currentUserId == null){
               this.currentUserId = id;
@@ -104,6 +122,10 @@ export default {
           //save current weekly list
           firebase.database().ref('users/' + id + '/weeklyList/').set({list}).then(() =>{
               console.log("Weekly list saved");
+              // save currently weekly completed list
+              firebase.database().ref('users/' + id + '/weeklyList/completed/').set({completedList}).then(() =>{
+                  console.log("Weekly completed list saved");
+              });
           });
       }
   }
